629a0f0cc081ce61841ac8fb5f7eafd0a6d62099
Unknown <alequis01@gmail.com>
2018-01-21 21:14:25 -0200

    Update curves, add object that follows the curve path

---
+++ Assets/5. Curves and Splines/Scripts/SplineWalker.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public enum SplineWalkerMode {
+	Once,
+	Loop,
+	PingPong
+}
+
+public class SplineWalker : MonoBehaviour {
+
+	public BezierSpline spline;
+	public float duration;
+	public bool lookForward;
+	public SplineWalkerMode mode;
+
+	private bool _goingForward = true;
+	private float _progress;
+
+	private void Update () {
+		if (_goingForward) {
+			_progress += Time.deltaTime / duration;
+			if (_progress > 1f) {
+				if (mode == SplineWalkerMode.Once) {
+					_progress = 1f;
+				}
+				else if (mode == SplineWalkerMode.Loop) {
+					_progress -= 1f;
+				}
+				else {
+					_progress = 2f - _progress;
+					_goingForward = false;
+				}
+			}
+		}
+		else {
+			_progress -= Time.deltaTime / duration;
+			if (_progress < 0f) {
+				_progress = -_progress;
+				_goingForward = true;
+			}
+		}
+		Vector3 position = spline.GetPoint(_progress);
+		transform.localPosition = position;
+		if (lookForward)
+			transform.LookAt(position + spline.GetDirection(_progress));
+		
+	}
+}
