c2cbad8f8eb0060f70ae67bb57aaf12ea0945aa1
Unknown <alequis01@gmail.com>
2018-01-23 16:40:51 -0200

    Update noise, add 1D, 2D and 3D noise

--- Assets/6. Noise/Scripts/Noise.cs
+++ Assets/6. Noise/Scripts/Noise.cs
@@ -1,10 +1,35 @@
 ﻿using UnityEngine;
 
+public delegate float NoiseMethod (Vector3 point, float frequency);
+
 public static class Noise {
+
+	public static NoiseMethod[] valueMethods = {
+		Value1D,
+		Value2D,
+		Value3D
+	};
 
 	/* represents a permutation table used as a hash function, that is, 
 	   transforms a value into some other value. */
 	private static int[] hash = {
+		151,160,137, 91, 90, 15,131, 13,201, 95, 96, 53,194,233,  7,225,
+		140, 36,103, 30, 69,142,  8, 99, 37,240, 21, 10, 23,190,  6,148,
+		247,120,234, 75,  0, 26,197, 62, 94,252,219,203,117, 35, 11, 32,
+		 57,177, 33, 88,237,149, 56, 87,174, 20,125,136,171,168, 68,175,
+		 74,165, 71,134,139, 48, 27,166, 77,146,158,231, 83,111,229,122,
+		 60,211,133,230,220,105, 92, 41, 55, 46,245, 40,244,102,143, 54,
+		 65, 25, 63,161,  1,216, 80, 73,209, 76,132,187,208, 89, 18,169,
+		200,196,135,130,116,188,159, 86,164,100,109,198,173,186,  3, 64,
+		 52,217,226,250,124,123,  5,202, 38,147,118,126,255, 82, 85,212,
+		207,206, 59,227, 47, 16, 58, 17,182,189, 28, 42,223,183,170,213,
+		119,248,152,  2, 44,154,163, 70,221,153,101,155,167, 43,172,  9,
+		129, 22, 39,253, 19, 98,108,110, 79,113,224,232,178,185,112,104,
+		218,246, 97,228,251, 34,242,193,238,210,144, 12,191,179,162,241,
+		 81, 51,145,235,249, 14,239,107, 49,192,214, 31,181,199,106,157,
+		184, 84,204,176,115,121, 50, 45,127,  4,150,254,138,236,205, 93,
+		222,114, 67, 29, 24, 72,243,141,128,195, 78, 66,215, 61,156,180,
+
 		151,160,137, 91, 90, 15,131, 13,201, 95, 96, 53,194,233,  7,225,
 		140, 36,103, 30, 69,142,  8, 99, 37,240, 21, 10, 23,190,  6,148,
 		247,120,234, 75,  0, 26,197, 62, 94,252,219,203,117, 35, 11, 32,
@@ -25,11 +50,31 @@ public static class Noise {
 
 	private const int HASH_MASK = 255;
 
-	public static float Value (Vector3 point, float frequency) {
+	public static float Value1D (Vector3 point, float frequency) {
 		point *= frequency;
 		int i = Mathf.FloorToInt(point.x);
 		i &= HASH_MASK;
 		return hash[i] * (1f / HASH_MASK);
+	}
+
+	public static float Value2D (Vector3 point, float frequency) {
+		point *= frequency;
+		int ix = Mathf.FloorToInt(point.x);
+		int iy = Mathf.FloorToInt(point.y);
+		ix &= HASH_MASK;
+		iy &= HASH_MASK;
+		return hash[hash[ix] + iy] * (1f / HASH_MASK);
+	}
+
+	public static float Value3D (Vector3 point, float frequency) {
+		point *= frequency;
+		int ix = Mathf.FloorToInt(point.x);
+		int iy = Mathf.FloorToInt(point.y);
+		int iz = Mathf.FloorToInt(point.z);
+		ix &= HASH_MASK;
+		iy &= HASH_MASK;
+		iz &= HASH_MASK;
+		return hash[hash[hash[ix] + iy] + iz] * (1f / HASH_MASK);
 	}
 
 }
--- Assets/6. Noise/Scripts/TextureCreator.cs
+++ Assets/6. Noise/Scripts/TextureCreator.cs
@@ -4,6 +4,8 @@ public class TextureCreator : MonoBehaviour {
 
 	[Range(2, 512)]
 	public int resolution = 256;
+	[Range(1, 3)]
+	public int dimensions = 3;
 	public float frequency = 1f;
 	private Texture2D _texture;
 
@@ -37,13 +39,14 @@ public class TextureCreator : MonoBehaviour {
 		Vector3 point01 = transform.TransformPoint(new Vector3(-0.5f, 0.5f));
 		Vector3 point11 = transform.TransformPoint(new Vector3( 0.5f, 0.5f));
 
+		NoiseMethod method = Noise.valueMethods[dimensions - 1];
 		float stepSize = 1f / resolution;
 		for (int y = 0; y < resolution; y++) {
 			Vector3 point0 = Vector3.Lerp(point00, point01, (y + 0.5f) * stepSize);
 			Vector3 point1 = Vector3.Lerp(point10, point11, (y + 0.5f) * stepSize);
 			for (int x = 0; x < resolution; x++) {
 				Vector3 point = Vector3.Lerp(point0, point1, (x + 0.5f) * stepSize);
-				_texture.SetPixel(x, y, Color.white * Noise.Value(point, frequency));
+				_texture.SetPixel(x, y, Color.white * method(point, frequency));
 			}
 		}
 		_texture.Apply();
