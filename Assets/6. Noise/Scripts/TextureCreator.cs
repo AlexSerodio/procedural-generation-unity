dd0833b82e1670855f10c15bae67f3cce3df11fb
Unknown <alequis01@gmail.com>
2018-01-23 15:05:20 -0200

    Update noise, add lattice noise

--- Assets/6. Noise/Scripts/TextureCreator.cs
+++ Assets/6. Noise/Scripts/TextureCreator.cs
@@ -4,6 +4,7 @@ public class TextureCreator : MonoBehaviour {
 
 	[Range(2, 512)]
 	public int resolution = 256;
+	public float frequency = 1f;
 	private Texture2D _texture;
 
 	void OnEnable () {
@@ -20,16 +21,29 @@ public class TextureCreator : MonoBehaviour {
 		FillTexture();
 	}
 
+	private void Update () {
+		if (transform.hasChanged) {
+			transform.hasChanged = false;
+			FillTexture();
+		}
+	}
+
 	public void FillTexture () {
 		if (_texture.width != resolution)
 			_texture.Resize(resolution, resolution);
 		
+		Vector3 point00 = transform.TransformPoint(new Vector3(-0.5f,-0.5f));
+		Vector3 point10 = transform.TransformPoint(new Vector3( 0.5f,-0.5f));
+		Vector3 point01 = transform.TransformPoint(new Vector3(-0.5f, 0.5f));
+		Vector3 point11 = transform.TransformPoint(new Vector3( 0.5f, 0.5f));
+
 		float stepSize = 1f / resolution;
 		for (int y = 0; y < resolution; y++) {
+			Vector3 point0 = Vector3.Lerp(point00, point01, (y + 0.5f) * stepSize);
+			Vector3 point1 = Vector3.Lerp(point10, point11, (y + 0.5f) * stepSize);
 			for (int x = 0; x < resolution; x++) {
-				//_texture.SetPixel(x, y, new Color(x * stepSize, y * stepSize, 0f));
-				//_texture.SetPixel(x, y, new Color((x + 0.5f) * stepSize, (y + 0.5f) * stepSize, 0f));
-				_texture.SetPixel(x, y, new Color((x + 0.5f) * stepSize % 0.1f, (y + 0.5f) * stepSize % 0.1f, 0f) * 10f);
+				Vector3 point = Vector3.Lerp(point0, point1, (x + 0.5f) * stepSize);
+				_texture.SetPixel(x, y, Color.white * Noise.Value(point, frequency));
 			}
 		}
 		_texture.Apply();
